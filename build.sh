01ac0feaf05433a229c4a9380d29dc3f86c33b4e
蔡明师 <cmsax@live.com>
2019-10-10 10:53:59 +0800

    udpate

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@ case "${TRAVIS_OS_NAME}" in
     osx)
         pyinstaller --name="CNKI-Converter-darwin" --windowed -F --icon ./icon.icns --osx-bundle-identifier cnki-converter main.py
         ;;
-    linux)
-        pyinstaller --name="CNKI-Converter-linux" --windowed -F main.py
-        ;;
     windows)
         pyinstaller --name="CNKI-Converter-windows" --windowed -F main.py
         ;;

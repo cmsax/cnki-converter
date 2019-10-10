7531f949492e0983d60fceb7ca805b68806e6686
蔡明师 <cmsax@live.com>
2019-10-10 13:22:33 +0800

    fix macOS dependencies

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 case "${TRAVIS_OS_NAME}" in
     osx)
+        cp -vf /usr/local/lib/python2.7/site-packages/shiboken2/libshiboken2-python2.7v.5.13.dylib /usr/local/lib/python2.7/site-packages/PySide2/
+        cp -vf /usr/local/lib/python2.7/site-packages/shiboken2/libshiboken2-python2.7v.5.13.dylib /usr/local/lib/python2.7/site-packages/PyInstaller/hooks/
         pyinstaller --name="CNKI-Converter-darwin" --windowed -F --icon ./icon.icns --osx-bundle-identifier cnki-converter main.py
         ;;
     windows)
